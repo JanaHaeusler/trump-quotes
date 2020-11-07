810d52d363df581abd43de4fbc8506f0cc6a6913
Jana Häusler <jana.m.haeusler@gmail.com>
2020-11-07 12:12:04 +0100

    Add delete logic to saved quotes page

--- src/QuotesSavedPage.js
+++ src/QuotesSavedPage.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components/macro'
+import useQuote from './Hooks/useQuote'
 
 export default function QuotesSavedPage({quotesOverview}) {
 
+    const {deleteQuote} = useQuote()
+
     return(
         <QuoteWrapper>
-            {quotesOverview.map( ({id, text, date}) => <li key={id}>{text} ({date})</li>)}
+            {quotesOverview.map( ({id, text, date}) => <li key={id}>{text} ({date})<span onClick={() => deleteQuote({id})}>&times;</span></li>)}
         </QuoteWrapper>
 
     )
@@ -15,16 +18,26 @@ const QuoteWrapper = styled.ul`
     width: 80%;
     margin: 0;
     padding: 0;
-    text-align: center;
     //overflow-y: scroll;
   
   li {
-   list-style: none;
-   margin-bottom: 20px;
-   padding: 15px;
-   border-radius: 5px;
-   box-shadow: 5px 5px 10px #d3d3d3;
-   background-color: papayawhip;
+    position: relative;
+    margin-bottom: 20px;
+    padding: 15px;
+    list-style: none;
+    border-radius: 5px;
+    box-shadow: 5px 5px 10px #d3d3d3;
+    background-color: papayawhip;
   }
-    
+
+  span {
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+    padding: 5px 10px;
+    border-radius: 5px;
+    box-shadow: 2px 2px 10px #A8A8A8;
+    background-color: teal;
+    color: white;
+  }
 `
